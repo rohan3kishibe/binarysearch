b19d66a988ccfdf600acb3025600cafdd29e0895
rohan <sagnikchatterjee24@gmail.com>
2021-07-31 20:03:40 +0530

    todo: work to be dome :sad:

---
+++ todo/p546.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class p546 {
+    public static int binarySearch(int[]nums ,int target,int low,int high){
+        //TODO 
+    }
+
+    public static int solve(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int[] nums = new int[p];
+        for (int i = 0; i < nums.length; i++) {
+            int p = sc.nextInt();
+            nums[i] = p;
+        }
+        System.out.println(solve(nums));
+    }
+
+}
